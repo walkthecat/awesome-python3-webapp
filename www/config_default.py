897034102bf6886a1c9aadb87b39fd8ed12607f0
walkthecat <58568819@qq.com>
2018-04-25 11:44:35 +0800

    添加相关文件

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456',
+        'db': 'dbtest'
+    },
+    'session': {
+        'secret': 'dbtest'
+    }
+}
